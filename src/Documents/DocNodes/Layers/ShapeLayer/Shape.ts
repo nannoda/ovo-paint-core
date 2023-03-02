cdf278709168b5fe34ca21cbdbea7b6d19af650f
NaN Noda <114621472+NaNnoda@users.noreply.github.com>
2023-03-02 12:03:42 -0600

    Update: shape

--- src/Documents/DocNodes/Layers/ShapeLayer/Shape.ts
+++ src/Documents/DocNodes/Layers/ShapeLayer/Shape.ts
@@ -2,6 +2,4 @@
 
 export abstract class Shape {
     abstract renderTo(e: OffscreenCanvasRenderingContext2D | CanvasRenderingContext2D): void;
-
-    abstract inRange(pos: Vec2): boolean;
 }
--- src/Documents/DocNodes/Layers/ShapeLayer/ShapeLayerNode.ts
+++ src/Documents/DocNodes/Layers/ShapeLayer/ShapeLayerNode.ts
@@ -1,34 +1,58 @@
 import {DocNode, DocNodeRenderEvent} from "../../DocNode";
 import {Shape} from "./Shape";
 
-export class ShapeLayerNode extends DocNode{
+
+export class ShapeLayerNode extends DocNode {
     constructor(name: string = "New Shape Layer", offset: Vec2 = [0, 0]) {
         super(name, offset);
     }
-    private shapes: Shape[] = [];
+
+    private _shapes: Shape[] = [];
+
     addShape(shape: Shape): void {
-        this.shapes.push(shape);
+        this._shapes.push(shape);
     }
 
     removeShape(shape: Shape): void {
-        const index = this.shapes.indexOf(shape);
+        const index = this._shapes.indexOf(shape);
         if (index !== -1) {
-            this.shapes.splice(index, 1);
+            this._shapes.splice(index, 1);
         }
     }
+
     render(e: DocNodeRenderEvent): void {
         // let bitmaps:CanvasImageSource = []
-        for (const shape of this.shapes) {
+        for (const shape of this._shapes) {
             shape.renderTo(e.ctx);
         }
         // e.ctx.drawImage(tmpCanvas, this.offset[0], this.offset[1]);
     }
-    getInRangeShapes(pos: Vec2): Shape | null {
-        for (const shape of this.shapes) {
-            if (shape.inRange(pos)) {
-                return shape;
-            }
-        }
-        return null;
+
+    get shapes(): Shape[] {
+        return this._shapes;
     }
+
+    // set shapes(shapes: Shape[]) {
+    //     this._shapes = shapes;
+    // }
+
+    // getInRangeShapes(pos: Vec2): Shape | null {
+    //     for (const shape of this.shapes) {
+    //         if (shape.inRange(pos)) {
+    //             return shape;
+    //         }
+    //     }
+    //     return null;
+    // }
+
+    // getShape(
+    //     tool: IShapeTool
+    // ): Shape | null {
+    //     for (const shape of this.shapes) {
+    //         if (tool.selectsShape(shape)) {
+    //             return shape;
+    //         }
+    //     }
+    //     return null;
+    // }
 }
