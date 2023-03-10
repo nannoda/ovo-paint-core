df9d02cc4e6c89a361470354903fb939dd977eb3
NaN Noda <114621472+NaNnoda@users.noreply.github.com>
2023-03-10 15:38:54 -0600

    Update Shape.ts

--- src/Documents/DocNodes/Layers/ShapeLayer/Shape.ts
+++ src/Documents/DocNodes/Layers/ShapeLayer/Shape.ts
@@ -1,7 +1,13 @@
+interface ShapeState {
 
+}
+export type CanvasCtx = OffscreenCanvasRenderingContext2D | CanvasRenderingContext2D;
 
-export abstract class Shape {
-    abstract renderTo(e: OffscreenCanvasRenderingContext2D | CanvasRenderingContext2D): void;
+export abstract class Shape<T extends ShapeState = ShapeState> {
+    abstract renderTo(e: CanvasCtx): void;
+
+    abstract getState(): T;
+    abstract applyState(state: T): void;
 
     // abstract fromJSON(json: any): void;
     // abstract toJSON(): string;
