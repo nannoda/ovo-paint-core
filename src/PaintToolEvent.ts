84c5fe54cc7e680f77778dd342dadcf2ad30a204
NaN Noda <114621472+NaNnoda@users.noreply.github.com>
2023-03-10 09:54:08 -0600

    Change paint tool event signature

--- src/PaintToolEvent.ts
+++ src/PaintToolEvent.ts
@@ -1,6 +1,7 @@
 import {DocNode} from "./Documents/DocNodes/DocNode";
 import {IUndoRedo} from "./Interface/IUndoRedo";
 import {OVODocument} from "./Documents/OVODocument";
+import {Vec2} from "./submodules/common-ts-utils/Math/Vector";
 
 export interface PaintToolEvent<NodeType extends DocNode> {
     pos: Vec2;
@@ -8,11 +9,11 @@ export interface PaintToolEvent<NodeType extends DocNode> {
     // type: "down" | "up" | "move";
     pressure: number;
     // doc: OVODocument;
-    ui: {
-        canvas: HTMLCanvasElement | OffscreenCanvas;
-        ctx: CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D;
-        scale: number;
-    },
+    // ui: {
+    //     canvas: HTMLCanvasElement | OffscreenCanvas;
+    //     ctx: CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D;
+    //     scale: number;
+    // },
     key: {
         shift: boolean;
         ctrl: boolean;
